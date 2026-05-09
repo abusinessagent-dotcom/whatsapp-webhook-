07bd33ab910c53e68955206750d6b5e52addd325
abusinessagent-dotcom <abusinessagent@gmail.com>
2026-05-09 19:10:42 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const VERIFY_TOKEN = "ramniwas123";
+
+app.get("/", (req, res) => {
+  res.send("Webhook running ✅");
+});
+
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    return res.status(200).send(challenge);
+  } else {
+    return res.sendStatus(403);
+  }
+});
+
+app.post("/webhook", (req, res) => {
+  console.log("DATA:", JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
